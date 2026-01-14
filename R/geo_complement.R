6d278e8e7c3f87069fa9a4a242eed07366104067
Fernando Cruz <fernandoilcruz@gmail.com>
2026-01-14 14:58:17 -0300

    update geo_complement

--- R/geo_complement.R
+++ R/geo_complement.R
@@ -4,17 +4,21 @@
 #'
 #' @format A data frame
 #' \describe{
-#'   \item{CodIBGE}{IBGE Code}
-#'   \item{Município}{Municipalities}
-#'   \item{Mesorregiões}{Mesorregions}
-#'   \item{Microrregião}{Microrregions}
-#'   \item{Região de Saúde}{State health department Regions}
+#'   \item{CODIGO IBGE}{IBGE Municipality Code}
+#'   \item{MUNICÍPIO}{Municipalities names}
+#'    \item{COREDE}{Regional Development Councils Regions}
+#'   \item{REGIÃO FUNCIONAL}{Planning Functional Regions}
+#'   \item{MESORREGIÃO}{IBGE 2017 Mesorregions}
+#'   \item{MICRORREGIÃO}{IBGE 2017 Microrregions}
+#'   \item{REGIÃO INTERMEDIÁRIA}{IBGE 2024 Intermediary Regions}
+#'   \item{REGIÃO IMEDIATA}{IBGE 2024 Immediate Regions}
 #'   \item{CRS}{Regional health coordination offices Regions}
-#'   \item{Coredes}{Regional Development Councils Regions}
-#'   \item{RF}{Planning Functional Regions}
+#'   \item{REGIÃO DE SAÚDE}{State health department Regions}
+#'   \item{MACRORREGIÃO DE SAÚDE}{State health department Macroregions}
+#'   \item{CRE}{Regional education coordination offices Regions}
 #' }
 #'
-#' @source State secretariats at the government of state of Rio Grande do Sul
+#' @source Department of Economics and Statistics (Planning, Governance and Management Office at the State Government of Rio Grande do Sul).
 #'
 #' @usage data(geo_complement)
 "geo_complement"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
-## code to prepare `DATASET` dataset goes here
+## Update data-raw when including new regionalization
 geo_complement <-
-  readxl::read_excel(".\\data-raw\\S1.1.xlsx")
+  readxl::read_excel(".\\data-raw\\geo_complement.xlsx")
 
 
 usethis::use_data(geo_complement, overwrite = TRUE)
